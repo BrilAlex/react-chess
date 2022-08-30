353c29b53f0e05ca95244e0d39fe0f707b736e0f
BrilAlex <brilalex.it@yandex.ru>
2022-08-30 16:57:05 +0300

    Board initialized

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,22 @@
-export const BoardComponent = () => {
+import {Board} from "../models/Board";
+import {FC, Fragment} from "react";
+import {CellComponent} from "./CellComponent";
+
+interface BoardProps {
+  board: Board
+  setBoard: (board: Board) => void
+}
+
+export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
   return (
     <div className={"board"}>
+      {board.cells.map((row, i) =>
+        <Fragment key={i}>
+          {row.map(cell =>
+            <CellComponent key={cell.id} cell={cell}/>
+          )}
+        </Fragment>
+      )}
     </div>
   );
 };
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,5 +1,12 @@
-export const CellComponent = () => {
+import {Cell} from "../models/Cell";
+import {FC} from "react";
+
+interface CellProps {
+  cell: Cell
+}
+
+export const CellComponent: FC<CellProps> = ({cell}) => {
   return (
-    <div className={"cell"}></div>
+    <div className={["cell", cell.color].join(" ")}></div>
   );
 };
