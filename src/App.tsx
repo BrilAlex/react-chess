9429fbd8674e57385a73d015ce7d681c75aadfbc
BrilAlex <brilalex.it@yandex.ru>
2022-09-01 10:22:00 +0300

    Player swap

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {BoardComponent} from "./components/BoardComponent";
 import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState<Player>(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState<Player>(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
@@ -15,11 +20,21 @@ function App() {
     newBoard.initCells();
     newBoard.addFigures();
     setBoard(newBoard);
+    setCurrentPlayer(whitePlayer);
+  }
+
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
   }
 
   return (
     <div className={"app"}>
-      <BoardComponent board={board} setBoard={setBoard}/>
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
     </div>
   );
 }
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -2,13 +2,18 @@ import {Board} from "../models/Board";
 import {FC, Fragment, useEffect, useState} from "react";
 import {CellComponent} from "./CellComponent";
 import {Cell} from "../models/Cell";
+import {Player} from "../models/Player";
 
 interface BoardProps {
   board: Board
   setBoard: (board: Board) => void
+  currentPlayer: Player | null
+  swapPlayer: () => void
 }
 
-export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+export const BoardComponent: FC<BoardProps> = (
+  {board, setBoard, currentPlayer, swapPlayer}
+) => {
   const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
   useEffect(() => {
@@ -19,8 +24,11 @@ export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
     if (selectedCell && selectedCell !== cell && selectedCell?.figure?.canMove(cell)) {
       selectedCell.moveFigure(cell);
       setSelectedCell(null);
+      swapPlayer();
     } else {
-      setSelectedCell(cell);
+      if (cell.figure?.color === currentPlayer?.color) {
+        setSelectedCell(cell);
+      }
     }
   }
 
@@ -35,19 +43,22 @@ export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
   }
 
   return (
-    <div className={"board"}>
-      {board.cells.map((row, i) =>
-        <Fragment key={i}>
-          {row.map(cell =>
-            <CellComponent
-              key={cell.id}
-              cell={cell}
-              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
-              onCellClick={onCellClick}
-            />
-          )}
-        </Fragment>
-      )}
+    <div>
+      <h3>Current player: {currentPlayer?.color}</h3>
+      <div className={"board"}>
+        {board.cells.map((row, i) =>
+          <Fragment key={i}>
+            {row.map(cell =>
+              <CellComponent
+                key={cell.id}
+                cell={cell}
+                selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                onCellClick={onCellClick}
+              />
+            )}
+          </Fragment>
+        )}
+      </div>
     </div>
   );
 };
